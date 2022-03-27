ee5c2fd40f8f59a0377e200b08416da84c831adf
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-03-27 06:39:45 +0530

    block: Import Xiaomi modifications
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -908,11 +908,14 @@ bool blk_rq_merge_ok(struct request *rq, struct bio *bio)
 
 enum elv_merge blk_try_merge(struct request *rq, struct bio *bio)
 {
+	enum elv_merge where;
+
 	if (blk_discard_mergable(rq))
-		return ELEVATOR_DISCARD_MERGE;
+		where = ELEVATOR_DISCARD_MERGE;
 	else if (blk_rq_pos(rq) + blk_rq_sectors(rq) == bio->bi_iter.bi_sector)
-		return ELEVATOR_BACK_MERGE;
+		where = ELEVATOR_BACK_MERGE;
 	else if (blk_rq_pos(rq) - bio_sectors(bio) == bio->bi_iter.bi_sector)
-		return ELEVATOR_FRONT_MERGE;
-	return ELEVATOR_NO_MERGE;
+		where = ELEVATOR_FRONT_MERGE;
+	where = ELEVATOR_NO_MERGE;
+	return where;
 }
