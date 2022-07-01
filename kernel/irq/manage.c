4086bcb4bd9f003bc0bb2f25539f4070b7fed0d2
Carlos Ayrton Lopez Arroyo <mail@carlosayrton.com>
2022-07-01 00:07:51 -0500

    kernel: irq: export symbol of the function irq_set_perf_affinity (ebbad4b)
    Export the symbol so out of tree modules can use this feature without binary mismatch with kernel

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1410,6 +1410,8 @@ void irq_set_perf_affinity(unsigned int irq, unsigned int perf_flag)
 	raw_spin_unlock_irqrestore(&desc->lock, flags);
 }
 
+EXPORT_SYMBOL(irq_set_perf_affinity);
+
 void unaffine_perf_irqs(void)
 {
 	struct irq_desc_list *data;
