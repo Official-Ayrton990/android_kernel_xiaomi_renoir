2af69a2c567a6933d031f8e738f983b2009905d8
Adrian Salido <salidoa@google.com>
2022-09-05 18:27:43 -0500

    drm/msm/dsi-staging: update backlight status on panel enable/disable
    Ensure that current backlight status is propagated to panel when it's
    turned on/off.
    
    [Ayrton: Adapted to CLO dsi/sde driver]
    
    Bug: 139655049
    Change-Id: Idcff6cfaaf46573d75844946c7d4ba353233f108
    Signed-off-by: Adrian Salido <salidoa@google.com>
    (cherry picked from commit 77542568d0b5ac8c1fe84d8ecbecb6dc83cdbfaf)
    Signed-off-by: Carlos Ayrton Lopez Arroyo <mail@carlosayrton.com>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -403,4 +403,6 @@ int dsi_panel_create_cmd_packets(const char *data, u32 length, u32 count,
 void dsi_panel_destroy_cmd_packets(struct dsi_panel_cmd_set *set);
 
 void dsi_panel_dealloc_cmd_packets(struct dsi_panel_cmd_set *set);
+
+int dsi_backlight_update_dpms(struct dsi_backlight_config *bl, int power_state);
 #endif /* _DSI_PANEL_H_ */
